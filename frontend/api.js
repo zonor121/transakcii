782654233fdfa01505a964edab0bbd9bba315d19
zonor121 <kopinator9000@gmail.com>
2026-09-11 11:30:33 +0500

    once again

--- frontend/api.js
+++
@@ -1,9 +0,0 @@
-import axios from 'axios';
-
-const api = axios.create({
-  baseURL: 'http://localhost:8000',
-});
-
-export const getParticipants = () => api.get('/participants').then(r => r.data);
-export const getTransfers = (limit = 50) => api.get(`/transfers?limit=${limit}`).then(r => r.data);
-export const doTransfer = (data) => api.post('/transfer', data).then(r => r.data);
